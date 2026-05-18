e8eb17140d949bc34e536281da20f41161b42469
Pravalli47 <cpravalli467@gmail.com>
2026-05-18 23:42:10 +0530

    Update UserController.java

--- src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -23,119 +23,128 @@ import com.jtspringproject.JtSpringProject.services.productService;
 @Controller
 public class UserController {
 
-	private final userService userService;
-	private final productService productService;
+    private final userService userService;
+    private final productService productService;
 
-	@Autowired
-	public UserController(userService userService, productService productService) {
-		this.userService = userService;
-		this.productService = productService;
-	}
+    @Autowired
+    public UserController(userService userService, productService productService) {
+        this.userService = userService;
+        this.productService = productService;
+    }
 
-	@GetMapping("/register")
-	public String registerUser() {
-		return "register";
-	}
+    @GetMapping("/")
+    public ModelAndView indexPage() {
+        ModelAndView mView = new ModelAndView("index");
 
-	@GetMapping("/buy")
-	public String buy() {
-		return "buy";
-	}
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        mView.addObject("username", username);
 
-	@GetMapping("/login")
-	public ModelAndView userLogin(@RequestParam(required = false) String error) {
-		ModelAndView mv = new ModelAndView("userLogin");
-		if ("true".equals(error)) {
-			mv.addObject("msg", "Please enter correct email and password");
-		}
-		return mv;
-	}
+        List<Product> products = this.productService.getProducts();
+        if (products.isEmpty()) {
+            mView.addObject("msg", "No products are available");
+        } else {
+            mView.addObject("products", products);
+        }
 
-	@GetMapping("/")
-	public ModelAndView indexPage() {
-		ModelAndView mView = new ModelAndView("index");
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
-		mView.addObject("username", username);
-		List<Product> products = this.productService.getProducts();
+        return mView;
+    }
 
-		if (products.isEmpty()) {
-			mView.addObject("msg", "No products are available");
-		} else {
-			mView.addObject("products", products);
-		}
-		return mView;
-	}
+    @GetMapping("/login")
+    public ModelAndView userLogin(@RequestParam(required = false) String error) {
+        ModelAndView mv = new ModelAndView("userLogin");
 
-	@GetMapping("/user/products")
-	public ModelAndView getProducts() {
+        if ("true".equals(error)) {
+            mv.addObject("msg", "Please enter correct email and password");
+        }
 
-		ModelAndView mView = new ModelAndView("uproduct");
+        return mv;
+    }
 
-		List<Product> products = this.productService.getProducts();
+    @GetMapping("/register")
+    public String registerUser() {
+        return "register";
+    }
 
-		if (products.isEmpty()) {
-			mView.addObject("msg", "No products are available");
-		} else {
-			mView.addObject("products", products);
-		}
+    @PostMapping("/newuserregister")
+    public ModelAndView registerNewUser(@ModelAttribute User user) {
 
-		return mView;
-	}
+        boolean exists = this.userService.checkUserExists(user.getUsername());
 
-	@PostMapping("newuserregister")
-	public ModelAndView registerNewUser(@ModelAttribute User user) {
-		boolean exists = this.userService.checkUserExists(user.getUsername());
+        if (!exists) {
+            user.setRole("ROLE_NORMAL");
+            this.userService.addUser(user);
+            return new ModelAndView("userLogin");
+        } else {
+            ModelAndView mView = new ModelAndView("register");
+            mView.addObject("msg", user.getUsername() + " is taken. Please choose a different username.");
+            return mView;
+        }
+    }
 
-		if (!exists) {
-			user.setRole("ROLE_NORMAL");
-			this.userService.addUser(user);
-			return new ModelAndView("userLogin");
-		} else {
-			ModelAndView mView = new ModelAndView("register");
-			mView.addObject("msg", user.getUsername() + " is taken. Please choose a different username.");
-			return mView;
-		}
-	}
+    @GetMapping("/buy")
+    public String buy() {
+        return "buy";
+    }
 
-	@GetMapping("/profileDisplay")
-	public String profileDisplay(Model model) {
+    @GetMapping("/user/products")
+    public ModelAndView getProducts() {
 
-		String username = SecurityContextHolder.getContext().getAuthentication().getName();
-		User user = userService.getUserByUsername(username);
+        ModelAndView mView = new ModelAndView("uproduct");
 
-		if (user != null) {
-			model.addAttribute("userid", user.getId());
-			model.addAttribute("username", user.getUsername());
-			model.addAttribute("email", user.getEmail());
-			model.addAttribute("password", "");
-			model.addAttribute("address", user.getAddress());
-		} else {
-			model.addAttribute("msg", "User not found");
-		}
+        List<Product> products = this.productService.getProducts();
+        if (products.isEmpty()) {
+            mView.addObject("msg", "No products are available");
+        } else {
+            mView.addObject("products", products);
+        }
 
-		return "updateProfile";
-	}
+        return mView;
+    }
 
-	@PostMapping("/updateuser")
-	public String updateUserProfile(@RequestParam("userid") int userid,
-			@RequestParam("username") String username,
-			@RequestParam("email") String email,
-			@RequestParam("password") String password,
-			@RequestParam("address") String address) {
-		User updatedUser = this.userService.updateUserProfile(userid, username, email, password, address);
-		if (updatedUser != null) {
-			refreshAuthenticatedPrincipal(username);
-		}
-		return "redirect:/";
-	}
+    @GetMapping("/profileDisplay")
+    public String profileDisplay(Model model) {
 
-	private void refreshAuthenticatedPrincipal(String username) {
-		Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
-		Authentication newAuthentication = new UsernamePasswordAuthenticationToken(
-				username,
-				currentAuthentication.getCredentials(),
-				currentAuthentication.getAuthorities());
-		SecurityContextHolder.getContext().setAuthentication(newAuthentication);
-	}
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.getUserByUsername(username);
 
+        if (user != null) {
+            model.addAttribute("userid", user.getId());
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            model.addAttribute("password", "");
+            model.addAttribute("address", user.getAddress());
+        } else {
+            model.addAttribute("msg", "User not found");
+        }
+
+        return "updateProfile";
+    }
+
+    @PostMapping("/updateuser")
+    public String updateUserProfile(@RequestParam("userid") int userid,
+                                    @RequestParam("username") String username,
+                                    @RequestParam("email") String email,
+                                    @RequestParam("password") String password,
+                                    @RequestParam("address") String address) {
+
+        User updatedUser = this.userService.updateUserProfile(userid, username, email, password, address);
+
+        if (updatedUser != null) {
+            refreshAuthenticatedPrincipal(username);
+        }
+
+        return "redirect:/";
+    }
+
+    private void refreshAuthenticatedPrincipal(String username) {
+        Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Authentication newAuthentication = new UsernamePasswordAuthenticationToken(
+                username,
+                currentAuthentication.getCredentials(),
+                currentAuthentication.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+    }
 }
